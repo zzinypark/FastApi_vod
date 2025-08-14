6d8b4a2eff006e0eb27128ad6ee62fc474e94230
zzinypark <zzinypark21@gamil.com>
2025-08-14 13:30:06 +0900

    CI

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> dict[str, str]:
     return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
-async def say_hello(name: str):
+async def say_hello(name: str) -> dict[str, str]:
     return {"message": f"Hello, {name}"}
---
+++ test_tamp.py
@@ -0,0 +1,12 @@
+from tamp import add
+
+
+def test_add() -> None:
+    # Given
+    a, b = 1, 1
+
+    # When
+    result = add(a, b)
+
+    # Then
+    assert result == 2
